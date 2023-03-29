6894b497922acbc4acb02074e47d05501ff6210e
LeeChangHee <lch7087@gmail.com>
2023-03-29 13:44:34 +0900

    Cors

--- src/main/java/com/example/silvertiger/controller/BookMarkController.java
+++ src/main/java/com/example/silvertiger/controller/BookMarkController.java
@@ -1,17 +1,11 @@
 package com.example.silvertiger.controller;
 
 import com.example.silvertiger.dto.BookMarkDto;
-import com.example.silvertiger.entity.Account;
-import com.example.silvertiger.entity.AccountBookMark;
-import com.example.silvertiger.entity.BookMark;
 import com.example.silvertiger.service.BookMarkService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 @RestController //즉 주용도는 JSON/XML형태로 객체 데이터 반환을 목적으로 합니다.
 @RequestMapping("/bookmark")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class BookMarkController {
 
     private final BookMarkService bookMarkService;
